eb88263f69333853731f9bd19550db28e3d4b6db
kimujae <dnwo0103@naver.com>
2023-03-24 18:28:06 +0900

    [ADD] using render response base.html in accountapp/view.py

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('안녕하세요!')
+    #return HttpResponse('안녕하세요!') # 리스폰스를 직접 만들어서 반환
+    return render(request, 'base.html')
