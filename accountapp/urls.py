ac296ec15dfbbc81ff9b040e0f4cc3cbe567677f
kimujae <dnwo0103@naver.com>
2023-03-25 17:13:47 +0900

    [ADD] CreateAccount view and template

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreate
 
 app_name = "accountapp" #경로 북마크와 같은 역할인듯
 urlpatterns = [
-    path('hello_world/', hello_world, name = 'hello_world') #주소, view, 라우트에 대한 대표 이름
+    path('hello_world/', hello_world, name = 'hello_world'), #주소, view, 라우트에 대한 대표 이름
+    path('create/', AccountCreate.as_view(), name = 'create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 # Create your views here.
 def hello_world(request):
     #return HttpResponse('안녕하세요!') # 리스폰스를 직접 만들어서 반환
     return render(request,'accountapp/helloworld.html')
+
+
+
+class AccountCreate(CreateView):
+    model = User # 모델
+    form_class = UserCreationForm # 폼
+    success_url = reverse_lazy('accountapp : hello_world') #계정생성 성공 시 반환 링크 / reverse를 사용하면 error가 뜸
+    template_name = 'accountapp/create.html'#템플릿 지정
+
